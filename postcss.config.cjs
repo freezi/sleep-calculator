e59fb003466f8966b8867742703779778bdea2cf
Kyler Riggs <100742516+KylerRiggs@users.noreply.github.com>
2022-11-03 07:56:10 -0400

    Add postcss-preset-env

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,10 @@
 module.exports = {
   plugins: {
+    // https://tailwindcss.com/docs/using-with-preprocessors
+    "tailwindcss/nesting": "postcss-nesting",
+    "postcss-preset-env": {
+      features: { "nesting-rules": false },
+    },
     tailwindcss: {},
     autoprefixer: {},
   },
